7d607ac4dd5268f365a0777bd45b09aaee179c91
Elvis Wanjiru <elvis.gatukui@student.moringaschool.com>
2022-10-05 21:43:31 +0300

    Completed

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+function App() {
+  const socialLinks = {
+    github: "https://github.com/liza",
+    linkedin: "https://www.linkedin.com/in/liza/",
+  };
+}
+
+function SocialMedia({ socialLinks: { github, linkedin } }) {
+  return (
+    <div>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+    </div>
+  );
+}
+
 function MovieCard({
   title,
-  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
 }) {
   return (
     <div className="movie-card">
@@ -13,5 +29,8 @@ function MovieCard({
     </div>
   );
 }
+  
+
+
 
 export default MovieCard;
